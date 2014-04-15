34b09caacb841f77cc2aa58a5012803838175600
ngaud <nicolas.gaud@utbm.fr>
2014-04-15 18:46:08 +0200

    Correction of the bug related to the destroy event
    Janus Issue #31

--- io.janusproject.kernel/src/main/java/io/janusproject/kernel/LifecycleSkill.java
+++ io.janusproject.kernel/src/main/java/io/janusproject/kernel/LifecycleSkill.java
@@ -21,8 +21,6 @@ package io.janusproject.kernel;
 
 import io.sarl.core.AgentKilled;
 import io.sarl.core.AgentSpawned;
-import io.sarl.core.Behaviors;
-import io.sarl.core.Destroy;
 import io.sarl.core.ExternalContextAccess;
 import io.sarl.core.Lifecycle;
 import io.sarl.lang.core.Agent;
@@ -32,20 +30,21 @@ import io.sarl.lang.core.Skill;
 
 import java.util.UUID;
 
-/** Skill that permits to manage the life cycle of the agents.
+/**
+ * Skill that permits to manage the life cycle of the agents.
  * 
  * @author $Author: srodriguez$
+ * @author $Author: ngaud$
  * @version $FullVersion$
  * @mavengroupid $GroupId$
  * @mavenartifactid $ArtifactId$
  */
 class LifecycleSkill extends Skill implements Lifecycle {
 
-	
 	private final SpawnService spawnService;
-	
-	
-	/** Constructs the skill.
+
+	/**
+	 * Constructs the skill.
 	 * 
 	 * @param agent - owner of the skill.
 	 * @param service - reference to the spawning service to use.
@@ -54,26 +53,27 @@ class LifecycleSkill extends Skill implements Lifecycle {
 		super(agent);
 		this.spawnService = service;
 	}
-	
+
 	@Override
 	protected void install() {
 		super.install();
 	}
-	
+
 	@Override
-	protected void uninstall() {	
+	protected void uninstall() {
 		super.uninstall();
 	}
 
 	@Override
-	public UUID spawnInContext(Class<? extends Agent> aAgent, AgentContext context,Object[] params) {
+	public UUID spawnInContext(Class<? extends Agent> aAgent, AgentContext context, Object[] params) {
 		UUID id = this.spawnService.spawn(context.getID(), aAgent, params);
-		fireAgentSpawned(aAgent,context);
+		fireAgentSpawned(aAgent, context);
 		return id;
 	}
-	
+
 	/**
 	 * Fire an {@link AgentSpawned} event to inform other context members of the creation of a new agent in the specified context.
+	 * 
 	 * @param parentContext - the context in which the owner agent has been spanwed
 	 */
 	protected void fireAgentSpawned(Class<? extends Agent> aAgent, AgentContext parentContext) {
@@ -87,40 +87,30 @@ class LifecycleSkill extends Skill implements Lifecycle {
 
 	@Override
 	public void killMe() {
-		fireDestroy();
+		// don't fire a destroy event here since it is not necessary to inform agent's internal behavior that will be done during the next call the killAgent
 		fireAgentKilledInAllCurrentContexts();
 		this.spawnService.killAgent(this.getOwner().getID());
 	}
-	
+
 	/**
-	 * Fire an {@link Destroy} event to inform owner agent's behaviors that this agent will die immediately
-	 * Normally it remains any members in this agents at this step
-	 */
-	protected void fireDestroy() {
-		Destroy event = new Destroy();				
-		getSkill(Behaviors.class).wake(event);
-	}
-	
-	/**
-	 * Fire an {@link AgentKilled} event in every single contexts to which the owner  agent belongs to
+	 * Fire an {@link AgentKilled} event in every single contexts to which the owner agent belongs to
 	 */
 	protected void fireAgentKilledInAllCurrentContexts() {
-		for(AgentContext context : getSkill(ExternalContextAccess.class).getAllContexts()) {
-			fireAgentLeft(context);
+		for (AgentContext context : getSkill(ExternalContextAccess.class).getAllContexts()) {
+			fireAgentKilled(context);
 		}
 	}
-	
+
 	/**
 	 * Fire an {@link AgentKilled} event to inform other context members of the death of the owner agent in the specified context.
 	 * @param parentContext - the default context of the owner agent
 	 */
-	protected void fireAgentLeft(AgentContext leftContext) {
+	protected void fireAgentKilled(AgentContext leftContext) {
 		EventSpace defSpace = leftContext.getDefaultSpace();
 		AgentKilled event = new AgentKilled();
 		event.setAgentID(this.getOwner().getID());
 		event.setSource(defSpace.getAddress(getOwner().getID()));
 		defSpace.emit(event);
 	}
-
 
 }
