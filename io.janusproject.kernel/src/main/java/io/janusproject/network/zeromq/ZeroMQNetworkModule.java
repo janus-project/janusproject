fe0ec46dacf3eada7ad4eb2db755d19e0f62aae9
sgalland <stephane.galland@utbm.fr>
2014-04-18 00:25:06 +0200

    Inject the EventSerializer with a Provider in place of the hard-coded injection in the ZeroMQ injection module. This enables to select the classname of the EventSerializer to inject from the system properties.

--- io.janusproject.kernel/src/main/java/io/janusproject/network/zeromq/ZeroMQNetworkModule.java
+++ io.janusproject.kernel/src/main/java/io/janusproject/network/zeromq/ZeroMQNetworkModule.java
@@ -52,24 +52,8 @@ public class ZeroMQNetworkModule extends AbstractModule {
 	protected void configure() {
 		bind(Network.class).to(ZeroMQNetwork.class).in(Singleton.class);
 
-		// Bind the serializer
-		Class<? extends EventSerializer> serializerType = GsonEventSerializer.class;
-		String serializerClassname = JanusConfig.getProperty(ZeroMQConfig.SERIALIZER_CLASSNAME);
-		if (serializerClassname!=null && !serializerClassname.isEmpty()) {
-			try {
-				Class<?> type = Class.forName(serializerClassname);
-				if (type!=null && EventSerializer.class.isAssignableFrom(type)) {
-					serializerType = type.asSubclass(EventSerializer.class);
-				}
-			}
-			catch(Throwable e) {
-				Logger.getAnonymousLogger().log(Level.SEVERE,
-						Locale.getString("CANNOT_CREATE_SERIALIZER", serializerClassname, ZeroMQConfig.SERIALIZER_CLASSNAME), e); //$NON-NLS-1$
-			}
-		}
-		bind(EventSerializer.class).to(serializerType).in(Singleton.class);
-		
-		
+		bind(EventSerializer.class).toProvider(SerializerProvider.class).in(Singleton.class);
+
 		bind(EventEncrypter.class).toProvider(EncrypterProvider.class).in(Singleton.class);
 
 		Multibinder<Service> uriBinder = Multibinder.newSetBinder(binder(), Service.class);
@@ -83,8 +67,54 @@ public class ZeroMQNetworkModule extends AbstractModule {
 		.setPrettyPrinting()
 		.create();
 	}
-	
-	
+
+	/**
+	 * @author $Author: sgalland$
+	 * @version $FullVersion$
+	 * @mavengroupid $GroupId$
+	 * @mavenartifactid $ArtifactId$
+	 */
+	private static class SerializerProvider extends AbstractSystemPropertyProvider<EventSerializer> {
+
+		private WeakReference<Injector> injector;
+		
+		/**
+		 */
+		public SerializerProvider() {
+			//
+		}
+
+		@Inject
+		void initialize(Injector injector) {
+			this.injector = new WeakReference<>(injector);
+		}
+
+		/** {@inheritDoc}
+		 */
+		@Override
+		public EventSerializer get() {
+			Class<? extends EventSerializer> serializerType = GsonEventSerializer.class;
+			String serializerClassname = getSystemProperty(ZeroMQConfig.SERIALIZER_CLASSNAME);
+			try {
+				if (serializerClassname!=null && !serializerClassname.isEmpty()) {
+					Class<?> type = Class.forName(serializerClassname);
+					if (type!=null && EventSerializer.class.isAssignableFrom(type)) {
+						serializerType = type.asSubclass(EventSerializer.class);
+					}
+				}
+				assert(this.injector!=null);
+				Injector inj = this.injector.get();
+				assert(inj!=null);
+				return inj.getInstance(serializerType);
+			}
+			catch (Throwable e) {
+				Logger.getAnonymousLogger().log(Level.SEVERE,
+						Locale.getString(ZeroMQNetworkModule.class, "CANNOT_CREATE_SERIALIZER", serializerClassname, ZeroMQConfig.SERIALIZER_CLASSNAME), e); //$NON-NLS-1$
+				return null;
+			}
+		}
+
+	}	
 
 	/**
 	 * @author $Author: sgalland$
